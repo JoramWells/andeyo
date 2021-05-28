3549f579623d0ed8ca34290909064e1b582b20ff
unknown <jorammanoah1@gmail.com>
2021-05-28 01:13:53 -0700

    Finished the search input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import { UserCircleIcon } from "@heroicons/react/solid";
+import { UserCircleIcon, XIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Home() {
   return (
@@ -20,10 +21,15 @@ export default function Home() {
         </div>
         <div>
           <form>
-            <input
-              placeholder="Search..."
-              className="border-none focus:outline-none"
-            />
+            <div className="flex w-full max-w-md rounded-full bg-yellow-200 items-center p-1">
+              <SearchIcon className="h-5 text-gray-500 hover:cursor-pointer m-1" />
+
+              <input
+                placeholder="Search..."
+                className="border-none focus:outline-none flex-grow bg-yellow-200 rounded-full p-1 text-sm text-gray-700"
+              />
+              <XIcon className="h-5 text-gray-500 m-1 hover:cursor-pointer" />
+            </div>
           </form>
         </div>
         {/* right */}
