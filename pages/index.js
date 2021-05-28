8d3cd7bf8d45163118407a5476dbc1fee67178b2
unknown <jorammanoah1@gmail.com>
2021-05-28 00:16:40 -0700

    Added tailwind config

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <meta name="description" content="Search EngineX" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex w-full p-5 justify-between">
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
         {/* left */}
         <div className="flex space-x-4 items-center">
           <p>Andeyo</p>
