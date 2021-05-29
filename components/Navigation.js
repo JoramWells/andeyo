1e2839d7afb3af9295b03be82decc26ed96a247a
unknown <jorammanoah1@gmail.com>
2021-05-29 02:04:39 -0700

    Tailwignng commit

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,7 +5,6 @@ import { XIcon, ChevronDownIcon } from "@heroicons/react/solid";
 import { SearchIcon } from "@heroicons/react/outline";
 import PopOver from "../components/PopOver";
 
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -13,141 +12,138 @@ function classNames(...classes) {
 export default function Navigation() {
   return (
     <>
-      <header className="flex w-full p-5 justify-between text-md text-gray-700 bg-yellow-300 shadow-sm">
-        <div className="flex space-x-4 items-center">
-          <b className="link">
-            <Link href="/">ANDEYO</Link>
-          </b>
-          <p className="link ">
-            <Link href="/documentation">Developers</Link>
-          </p>
-          <p className="link">
-            <Link href="/about">Pricing</Link>
-          </p>
-          <p className="link">
-            <Link href="/about">Partners</Link>
-          </p>
-          <p>
-            <Link href="/about">
-              <PopOver/>
-            </Link>
-          </p>
+        <nav className=" top-0 flex flex-wrap w-full p-5 justify-between text-md text-gray-700 bg-yellow-300 shadow-sm">
+          <div className=" container flex flex-wrap space-x-4 items-center">
+            <b className="link text-lg">
+              <Link href="/">ANDEYO</Link>
+            </b>
+            <p className="link ">
+              <Link href="/documentation">Developers</Link>
+            </p>
+            <p className="link">
+              <Link href="/about">Pricing</Link>
+            </p>
+            <p className="link">
+              <Link href="/about">Partners</Link>
+            </p>
+            <p>
+                <PopOver />
+            </p>
+          </div>
+          <div>
+            <form>
+              <div className="flex flex-row w-full max-w-md rounded-full bg-yellow-200 items-center p-1 sm:max-w-xl lg:max-w-2xl">
+                <SearchIcon className="h-5 text-gray-500 hover:cursor-pointer m-1" />
 
-        </div>
-        <div>
-          <form>
-            <div className="flex flex-row w-full max-w-md rounded-full bg-yellow-200 items-center p-1 sm:max-w-xl lg:max-w-2xl">
-              <SearchIcon className="h-5 text-gray-500 hover:cursor-pointer m-1" />
-
-              <input
-                placeholder="Search..."
-                className="border-none focus:outline-none flex-grow bg-yellow-200 rounded-full p-1 text-sm text-gray-700"
-              />
-              <XIcon className="h-5 text-gray-500 m-1 hover:cursor-pointer" />
-            </div>
-          </form>
-        </div>
-        {/* right */}
-        <div className="flex space-x-4 items-center ">
-          <p className="link">
-            <Menu as="div" className="relative inline-block text-left">
-              {({ open }) => (
-                <>
-                  <div>
-                    <Menu.Button className="inline-flex justify-center w-full  shadow-sm px-4 py-2  text-gray-700  focus:outline-none  ">
-                      Register
-                      <ChevronDownIcon
-                        className="-mr-1  mt-1 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    show={open}
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items
-                      static
-                      className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                <input
+                  placeholder="Search..."
+                  className="border-none focus:outline-none flex-grow bg-yellow-200 rounded-full p-1 text-sm text-gray-700"
+                />
+                <XIcon className="h-5 text-gray-500 m-1 hover:cursor-pointer" />
+              </div>
+            </form>
+          </div>
+          {/* right */}
+          <div className="flex space-x-4 items-center ">
+            <p className="link">
+              <Menu as="div" className="relative inline-block text-left">
+                {({ open }) => (
+                  <>
+                    <div>
+                      <Menu.Button className="inline-flex justify-center w-full  shadow-sm px-4 py-2  text-gray-700  focus:outline-none  ">
+                        Register
+                        <ChevronDownIcon
+                          className="-mr-1  mt-1 h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
                     >
-                      <div className="py-1">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active
-                                  ? "bg-gray-100 text-gray-900"
-                                  : "text-gray-700",
-                                "block px-4 py-2 text-sm"
-                              )}
-                            >
-                              Account settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active
-                                  ? "bg-gray-100 text-gray-900"
-                                  : "text-gray-700",
-                                "block px-4 py-2 text-sm"
-                              )}
-                            >
-                              Support
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active
-                                  ? "bg-gray-100 text-gray-900"
-                                  : "text-gray-700",
-                                "block px-4 py-2 text-sm"
-                              )}
-                            >
-                              License
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <form method="POST" action="#">
+                      <Menu.Items
+                        static
+                        className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                      >
+                        <div className="py-1">
                           <Menu.Item>
                             {({ active }) => (
-                              <button
-                                type="submit"
+                              <a
+                                href="#"
                                 className={classNames(
                                   active
                                     ? "bg-gray-100 text-gray-900"
                                     : "text-gray-700",
-                                  "block w-full text-left px-4 py-2 text-sm"
+                                  "block px-4 py-2 text-sm"
                                 )}
                               >
-                                Sign out
-                              </button>
+                                Account settings
+                              </a>
                             )}
                           </Menu.Item>
-                        </form>
-                      </div>
-                    </Menu.Items>
-                  </Transition>
-                </>
-              )}
-            </Menu>
-          </p>
-        </div>
-      </header>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active
+                                    ? "bg-gray-100 text-gray-900"
+                                    : "text-gray-700",
+                                  "block px-4 py-2 text-sm"
+                                )}
+                              >
+                                Support
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active
+                                    ? "bg-gray-100 text-gray-900"
+                                    : "text-gray-700",
+                                  "block px-4 py-2 text-sm"
+                                )}
+                              >
+                                License
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <form method="POST" action="#">
+                            <Menu.Item>
+                              {({ active }) => (
+                                <button
+                                  type="submit"
+                                  className={classNames(
+                                    active
+                                      ? "bg-gray-100 text-gray-900"
+                                      : "text-gray-700",
+                                    "block w-full text-left px-4 py-2 text-sm"
+                                  )}
+                                >
+                                  Sign out
+                                </button>
+                              )}
+                            </Menu.Item>
+                          </form>
+                        </div>
+                      </Menu.Items>
+                    </Transition>
+                  </>
+                )}
+              </Menu>
+            </p>
+          </div>
+        </nav>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,40 @@ import Navigation from "../components/Navigation";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home | Andeyo</title>
-        <meta name="description" content="Search EngineX" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <head>
+        <Head>
+          <title>Home | Andeyo</title>
+          <meta name="description" content="Search EngineX" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          />
+        </Head>
+      </head>
+
+      {/* Include Navbar */}
+
       <Navigation />
       <main>
         <div
-          className="p-5 flex flex-row justify-around space-x-4 "
-          style={{ height: "500px" }}
+          className="relative pt-5 content-center items-center flex justify-around space-x-4 "
+          style={{ minHeight: "75vh" }}
         >
           <div>
+            <h1 className="text-3xl leading-normal font-extrabold text-gray-900 p-5">
+              The flexible AI-powered <br /> Search & Discovery <br /> Platform
+            </h1>
+            <p className="text-gray-700 p-5" style={{ fontSize: "1.5rem" }}>
+              Create cutting-edge UX for <br /> web, apps & ecommerce
+            </p>
+            <div className="space-x-4 mt-2 text-gray-700 p-5">
+              <button className="btn">Start for free</button>
+              <button className="btn">Request Demo</button>
+            </div>
+          </div>
+
+          <div className="container relative mx-auto">
             <h1
               style={{ fontSize: "2.5rem" }}
               className="text-3xl font-extrabold text-gray-900 p-5"
@@ -31,68 +53,79 @@ export default function Home() {
               <button className="btn">Request Demo</button>
             </div>
           </div>
-
-          <div>
-          <h1
-              style={{ fontSize: "2.5rem" }}
-              className="text-3xl font-extrabold text-gray-900 p-5"
-            >
-              The flexible AI-powered <br /> Search & Discovery <br /> Platform
-            </h1>
-            <p className="text-gray-700 p-5" style={{ fontSize: "1.5rem" }}>
-              Create cutting-edge UX for <br /> web, apps & ecommerce
-            </p>
-            <div className="space-x-4 mt-2 text-gray-700 p-5">
-              <button className="btn">Start for free</button>
-              <button className="btn">Request Demo</button>
-            </div>
-          </div>
         </div>
-        <div className="flex p-2 justify-around items-center ml-auto mr-auto max-w-6xl" style={{height:"300px"}}>
-          <div>
-            <h2 style={{ fontSize: "1.5rem" }}>Developers</h2>
-            <p className="text-gray-500 text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis
-            </p>
-          </div>
-          <div>
-            <h2 style={{ fontSize: "1.5rem" }}>Ecommerce</h2>
-            <p className="text-gray-500 text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis
-            </p>
-          </div>
-          <div>
-            <h2 style={{ fontSize: "1.5rem" }}>Media</h2>
-            <p className="text-gray-500 text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis
-            </p>
+        <div
+          className="flex p-2 container justify-around items-center ml-auto mr-auto max-w-6xl"
+          style={{ height: "50vh" }}
+        >
+          <div className="flex flex-wrap">
+            {/* 1 */}
+            <div className="lg:pt-12 w-full md:w-4/12 px-4">
+              <div className="relative p-5 min-w-0 break-words rounded-lg w-full shadow-lg bg-white">
+                <div className="px-4 py-5 flex-auto">
+                  <div className="p-3 inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-yellow-500">
+                    <i className="fas fa-award"></i>
+                  </div>
+                  <h6 className="text-xl mt-4 font-semibold">Awarded Agency</h6>
+                  <p className="text-gray-500 text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maxime mollitia, molestiae quas vel sint commodi repudiandae
+                  consequuntur voluptatum laborum numquam blanditiis
+                </p>
+                </div>
+              </div>
+            </div>
+            {/* 2 */}
+            <div className="lg:pt-12 w-full md:w-4/12 px-4">
+              <div className="relative p-5 min-w-0 break-words rounded-lg w-full shadow-lg bg-white">
+                <div className="px-4 py-5 flex-auto">
+                  <div className="p-3 inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-yellow-500">
+                    <i className="fas fa-award"></i>
+                  </div>
+                  <h6 className="text-xl mt-4 font-semibold">Awarded Agency</h6>
+                  <p className="text-gray-500 text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maxime mollitia, molestiae quas vel sint commodi repudiandae
+                  consequuntur voluptatum laborum numquam blanditiis
+                </p>
+                </div>
+              </div>
+            </div>
+            {/* 3 */}
+            <div className="lg:pt-12 w-full md:w-4/12 px-4">
+              <div className="relative p-5 min-w-0 break-words rounded-lg w-full shadow-lg bg-white">
+                <div className="px-4 py-5 flex-auto">
+                  <div className="p-3 inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-yellow-500">
+                    <i className="fas fa-award"></i>
+                  </div>
+                  <h6 className="text-xl mt-4 font-semibold">Awarded Agency</h6>
+                  <p className="text-gray-500 text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Maxime mollitia, molestiae quas vel sint commodi repudiandae
+                  consequuntur voluptatum laborum numquam blanditiis
+                </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div
           style={{ height: "400px" }}
           className="bg-yellow-200 h-200 p-3 flex flex-row items-center"
         >
-          <div style={{margin:"auto", display:"block"}}>
-          <h1
-                style={{ fontSize: "2rem" }}
-                className="text-center text-3xl font-extrabold text-gray-900"
-              >
-                <b>Deliver what your customers want every time</b>{" "}
-              </h1>
-              <p className="text-center text-gray-600 md:max-w-3xl">
-                Algolia’s search-as-a-service and full suite of APIs allow teams
-                to easily develop tailored, fast Search & Discovery experiences
-                that delight and convert.
-              </p>
+          <div style={{ margin: "auto", display: "block" }}>
+            <h1
+              style={{ fontSize: "2rem" }}
+              className="text-center text-3xl font-extrabold text-gray-900"
+            >
+              <b>Deliver what your customers want every time</b>{" "}
+            </h1>
+            <p className="text-center text-gray-600 md:max-w-3xl">
+              Algolia’s search-as-a-service and full suite of APIs allow teams
+              to easily develop tailored, fast Search & Discovery experiences
+              that delight and convert.
+            </p>
           </div>
-
-
         </div>
       </main>
       <Footer />
